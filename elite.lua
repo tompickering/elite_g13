88ef32fa55e4ef7434000b8abd4a5f00dae3954b
Tom Pickering <tom@alephspace.net>
2024-12-08 18:38:01 +0000

    Implement jump logic in lua

--- elite.lua
+++ elite.lua
@@ -1,5 +1,45 @@
-function reset(t)
-    reset_lcd()
+function is_in(t, val)
+    for k, v in ipairs(t) do
+        if v == val then
+            return true
+        end
+    end
+
+    return false
+end
+
+stars_scoopable = {'O', 'B', 'A', 'F', 'G', 'K', 'M'}
+stars_danger = {'D', 'DA', 'DB', 'DC', 'DO', 'DQ', 'DX'}
+
+function reset(t) reset_lcd() end
+
+jumps_remaining = 0
+
+function FSDTarget(t)
+    jumps_remaining = t['RemainingJumpsInRoute']
+end
+
+function StartJump(t)
+    if t['JumpType'] == 'Hyperspace' then
+        star_system = t['StarSystem']
+        star_class = t['StarClass']
+
+        jumps_remaining_str = tostring(jumps_remaining)
+
+        if is_in(stars_danger, star_class) then
+            set_color(0xff, 0x00, 0x00)
+        elseif is_in(stars_scoopable, star_class) then
+            set_color(0x00, 0xff, 0x00)
+        else
+            set_color(0x00, 0x00, 0xff)
+        end
+
+        clear_lcd()
+        draw_string(60, 2, "JUMPING");
+        draw_string(4, 10, star_system)
+        draw_string(4, 20, star_class)
+        draw_string(4, 30, tjumps_remaining_str)
+    end
 end
 
 function DockingGranted(t)
