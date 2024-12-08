a8047c7dedd59645967521b3e80a24400d9cc883
Tom Pickering <tom@alephspace.net>
2024-12-08 16:52:15 +0000

    Update DockingGranted in lua

--- elite.lua
+++ elite.lua
@@ -1,5 +1,9 @@
 function DockingGranted(t)
-    print("Docking granted on pad " .. t['LandingPad'])
     set_color(0x00, 0xff, 0x00)
     draw_string_scaled(36, 2, "GRANTED", 2);
+    landing_pad = tostring(t['LandingPad'])
+    if #landing_pad < 2 then
+        landing_pad = '0' .. landing_pad
+    end
+    draw_string_scaled(60, 18, landing_pad, 3)
 end
