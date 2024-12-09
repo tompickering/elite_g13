2b8175193322b436060440e0ff13335941cab93e
Tom Pickering <tom@alephspace.net>
2024-12-09 13:52:18 +0000

    DockingGranted refinements

--- elite.lua
+++ elite.lua
@@ -87,12 +87,12 @@ end
 function DockingGranted(t)
     clear()
     set_color(0x00, 0xff, 0x00)
-    draw_string_scaled(36, 2, "GRANTED", 2);
+    draw_string_scaled(36, 4, "GRANTED", 2);
     landing_pad = tostring(t['LandingPad'])
     if #landing_pad < 2 then
         landing_pad = '0' .. landing_pad
     end
-    draw_string_scaled(60, 18, landing_pad, 3)
+    draw_string_scaled(60, 20, landing_pad, 3)
 end
 
 function DockingDenied(t)
