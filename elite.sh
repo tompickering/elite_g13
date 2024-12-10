9cdfa3c7556c96f35faa23a365ad198d0389b2d9
Tom Pickering <tom@alephspace.net>
2024-12-10 00:55:54 +0000

    Search for a new journal file if the current one is closed

--- elite.sh
+++ elite.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
+#!/bin/zsh
 
 dir="$(dirname $0)"
 elitepath="${dir}/elite"
 
 journal_dir="${1:-/home/tom/games/elite_log}"
 
-journal="$(find "$journal_dir"/ -type f -name Journal.\* -exec stat --format "%Y %n" {} \; | sort -nr | cut -d ' ' -f 2- | head -n 1)"
+while true; do
+    journal="$(find "$journal_dir"/ -type f -name Journal.\* -exec stat --format "%Y %n" {} \; | sort -nr | cut -d ' ' -f 2- | head -n 1)"
 
-if [ -z $journal ]; then
-    echo "Could not find journal in '$journal_dir'"
-    exit 1
+    if [ -z $journal ]; then
+        echo "Could not find journal in '$journal_dir'"
+        exit 1
 
-fi
+    fi
 
-echo "Found journal '$journal'"
+    echo "Found journal '$journal'"
 
-tail -f "$journal" | dos2unix | $elitepath $journaldir
+    (inotifywait -e close "$journal" && kill -TERM $(pidof elite) && kill $(pidof tail))&
+    tail -f -n0 "$journal" | dos2unix | $elitepath $journaldir
+done
